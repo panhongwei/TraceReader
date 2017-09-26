0d2aec9a3ef637d64220d25fe23b8e39c1b7d893
panda <phw19901222@163.com>
2017-09-26 11:12:37 +0800

    Fix Issue:fix an isusse

--- TraceReader/src/com/panda/trace/BytesHelper.java
+++ TraceReader/src/com/panda/trace/BytesHelper.java
@@ -41,23 +41,23 @@ public class BytesHelper {
     //ptr[2] | (ptr[3] << 8) | (ptr[4] << 16) | (ptr[5] << 24);
     public static int read2LE(byte[] data,int offset){
     	return (int)((data[offset]&0xff)|
-    				((data[offset+1]&0xff)<<8));
+				((data[offset+1]&0xff)<<8))&0xffff;
     }
     public static long read4LE(byte[] data,int offset){
-    	return (long)((data[offset]&0xff)|
-    				((data[offset+1]&0xff)<<8)|
-    				((data[offset+2]&0xff)<<16)|
-    				((data[offset+3]&0xff)<<24));
+    	return ((long)((data[offset]&0xff)|
+				((data[offset+1]&0xff)<<8)|
+				((data[offset+2]&0xff)<<16)|
+				((data[offset+3]&0xff)<<24)))&0xffffffffl;
     }
     public static long read8LE(byte[] data,int offset){
-    	return (long)((data[offset]&0xff)|
-    				((data[offset+1]&0xff)<<8)|
-    				((data[offset+2]&0xff)<<16)|
-    				((data[offset+3]&0xff)<<24)|
-    				((data[offset+4]&0xff)<<32)|
-    				((data[offset+5]&0xff)<<40)|
-    				((data[offset+6]&0xff)<<48)|
-    				((data[offset+7]&0xff)<<56));
+    	return ((long)(data[offset]&0xff)|
+				((data[offset+1]&0xff)<<8)|
+				((data[offset+2]&0xff)<<16)|
+				((data[offset+3]&0xff)<<24)|
+				((data[offset+4]&0xff)<<32)|
+				((data[offset+5]&0xff)<<40)|
+				((data[offset+6]&0xff)<<48)|
+				((data[offset+7]&0xff)<<56))&0xffffffffffffffffl;
     }
 
 }
